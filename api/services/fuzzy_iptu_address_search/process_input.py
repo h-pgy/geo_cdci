c5db792c8b9a90d0496eb6d3c72828993bbb7fa5
Henrique Pougy <hpougy@gmail.com>
2026-05-05 09:45:15 -0300

    tratando tipo de logradouro

--- api/services/fuzzy_iptu_address_search/process_input.py
+++ api/services/fuzzy_iptu_address_search/process_input.py
@@ -3,12 +3,31 @@ import re
 
 class AddressStringProcessor:
 
+    mapper_tipos_logradouro = {
+            "RUA": "R",
+            "AVENIDA": "AV",
+            "ALAMEDA": "AL",
+            "TRAVESSA": "TV",
+            "LARGO": "LG",
+            "PRACA": "PC",
+            "VILA": "VL",
+            "VIADUTO": "VD",
+            "PARQUE": "PQ",
+            "BECO": "BC",
+            "ESTRADA": "ES",
+            "RODOVIA": "RP",
+            "LADEIRA": "LD",
+            "CAMINHO": "CM",
+            "PASSAGEM": "PS"
+        }
+
     def __init__(self):
             # Pipeline de métodos a serem executados na ordem
             self.pipeline = [
                 self.to_upper,
                 self.remove_accents,
                 self.clean_special_chars,
+                self.normalize_address_type,
                 self.strip_spaces
             ]
 
@@ -22,6 +41,17 @@ class AddressStringProcessor:
     def clean_special_chars(self, text: str) -> str:
         # Mantém apenas letras, números e espaços
         return re.sub(r'[^A-Z0-9\s]', ' ', text)
+    
+    def normalize_address_type(self, text: str) -> str:
+        """
+        Substitui nomes de logradouros por suas abreviações oficiais da PMSP.
+        """
+        for full_type, abbreviation in self.mapper_tipos_logradouro.items():
+            # \b garante que estamos trocando a palavra inteira no início da string
+            # Ex: "RUA AUGUSTA" -> "R AUGUSTA"
+            pattern = rf"\b{full_type}\b"
+            text = re.sub(pattern, abbreviation, text)
+        return text
 
     def strip_spaces(self, text: str) -> str:
         return " ".join(text.split())
